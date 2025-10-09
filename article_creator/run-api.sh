8ca79042d142e4bc63b57f68a2e60b69703b70a9
Paweł Kędzia <pawel@radlab.dev>
2025-10-09 13:49:17 +0200

    Add manage.py and run-api.sh scripts for article_creator Django app

---
+++ article_creator/run-api.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PLAYGROUND_PUBLIC_USER_NAME=ULFqX4RxVKX3Xy8aiw7
+export PLAYGROUND_PUBLIC_USER_PASS=xu6qdKGvh49b9f9ddY3
+
+export TOKENIZERS_PARALLELISM=true
+
+python3 manage.py runserver 0.0.0.0:8200
