4edb9bae9e66ce924102a29c20d9636897c3619c
pawkedzia <pawel@radlab.dev>
2026-08-18 03:27:07 +0200

    Add multithreading support for 3-class polarity check: implement `check_polarity_3c_parallel`, add worker threads and configurable `num_workers`.

--- article_creator/apps_creator/periodic/check_news_polarity_for_public_stream.py
+++ article_creator/apps_creator/periodic/check_news_polarity_for_public_stream.py
@@ -6,12 +6,27 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 django.setup()
 
 from general.constants import DEFAULT_MODELS_CONFIG
+from apps_creator.periodic.src.utils import prepare_parser
 
 from system.controllers import SystemController
 from creator.controllers.polarity import PolarityController
 
 
+def prepare_polarity_parser(argv=None):
+    parser = prepare_parser(argv)
+    parser.add_argument(
+        "--num-workers",
+        dest="num_workers",
+        type=int,
+        default=1,
+        help="Number of workers for parallel news polarity checking.",
+    )
+    return parser
+
+
 def main(argv=None):
+    args = prepare_polarity_parser(argv).parse_args(argv)
+
     system_settings = SystemController.get_system_settings()
     if system_settings.doing_news_polarity_3c_check:
         return
@@ -20,15 +35,15 @@ def main(argv=None):
 
     try:
         polarity_controller = PolarityController(
-            models_config_path=DEFAULT_MODELS_CONFIG,
+            models_config_path=args.json_config or DEFAULT_MODELS_CONFIG,
             add_to_db=True,
         )
 
         news_without_polarity = polarity_controller.get_news_without_polarity_3c()
-        for news in news_without_polarity:
-            res = polarity_controller.check_polarity_3c(news_list=[news])
-            polarity_str = res[0].get("polarity", res[0].get("label")) if res else None
-            logging.info(f"Checked 3c polarity for news {news.pk} => {polarity_str}")
+        polarity_controller.check_polarity_3c_parallel(
+            news_list=news_without_polarity,
+            num_workers=args.num_workers,
+        )
     except Exception as e:
         logging.error(e)
 
