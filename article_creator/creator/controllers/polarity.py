73f1cb22f6f85e91c0dbf8bf22c71c81fc8b01bd
pawkedzia <pawel@radlab.dev>
2026-08-23 19:31:40 +0200

    Comment out unused `check_polarity_3c_local` method and remove `emotion_polarity_3c` configuration.

--- article_creator/creator/controllers/polarity.py
+++ article_creator/creator/controllers/polarity.py
@@ -15,7 +15,7 @@ from general.controllers.models import ModelsConfigController
 
 class PolarityController(ModelsConfigController):
     JSON_MAIN_FIELD = "emotion_polarity"
-    JSON_MAIN_FIELD_C3_POLARITY = "emotion_polarity_3c"
+    # JSON_MAIN_FIELD_C3_POLARITY = "emotion_polarity_3c"
     POLARITY_CHECK_3C_EP = "check_3c_polarity"
 
     LLM_ROUTER_HOST = "llm_router_host"
@@ -103,56 +103,56 @@ class PolarityController(ModelsConfigController):
         Backwards-compatible wrapper for check_polarity_3c
         """
         return self.check_polarity_3c(news_list=news_list)
-
-    def check_polarity_3c_local(
-        self, news_list: List[GeneratedNews]
-    ) -> List[dict] | None:
-        """
-        Check 3-class polarity using local model endpoint (previous implementation)
-        :param news_list: List of GeneratedNews objects
-        :return: List of dicts with polarity results or None on error
-        """
-        logging.info("Checking 3c polarity (local)")
-        if not news_list:
-            return []
-
-        ep_url = self._get_ep_host(
-            self._m2e2hosts[self.JSON_MAIN_FIELD_C3_POLARITY][
-                self.POLARITY_CHECK_3C_EP
-            ]
-        )
-
-        texts_to_check = [n.generated_text for n in news_list]
-        data = {"texts": texts_to_check}
-
-        response = BasePublicApiInterface.general_call_post(
-            host_url=None,
-            endpoint=ep_url,
-            data=None,
-            json_data=data,
-            headers=self.API_HEADER,
-            login_url=None,
-        )
-
-        if not response or "response" not in response:
-            self._last_response = response
-            return None
-
-        news_polarity_response = response["response"]
-        self._last_response = response
-
-        if self.add_to_db:
-            for idx, news in enumerate(news_list):
-                if idx < len(news_polarity_response):
-                    item = news_polarity_response[idx]
-                    text_3c = item.get("text", item.get("original", None))
-                    assert news.generated_text == text_3c
-                    label_3c = item.get("label", item.get("polarity", None))
-                    GeneratedNews.objects.filter(pk=news.pk).update(
-                        polarity_3c=label_3c
-                    )
-
-        return news_polarity_response
+    #
+    # def check_polarity_3c_local(
+    #     self, news_list: List[GeneratedNews]
+    # ) -> List[dict] | None:
+    #     """
+    #     Check 3-class polarity using local model endpoint (previous implementation)
+    #     :param news_list: List of GeneratedNews objects
+    #     :return: List of dicts with polarity results or None on error
+    #     """
+    #     logging.info("Checking 3c polarity (local)")
+    #     if not news_list:
+    #         return []
+    #
+    #     ep_url = self._get_ep_host(
+    #         self._m2e2hosts[self.JSON_MAIN_FIELD_C3_POLARITY][
+    #             self.POLARITY_CHECK_3C_EP
+    #         ]
+    #     )
+    #
+    #     texts_to_check = [n.generated_text for n in news_list]
+    #     data = {"texts": texts_to_check}
+    #
+    #     response = BasePublicApiInterface.general_call_post(
+    #         host_url=None,
+    #         endpoint=ep_url,
+    #         data=None,
+    #         json_data=data,
+    #         headers=self.API_HEADER,
+    #         login_url=None,
+    #     )
+    #
+    #     if not response or "response" not in response:
+    #         self._last_response = response
+    #         return None
+    #
+    #     news_polarity_response = response["response"]
+    #     self._last_response = response
+    #
+    #     if self.add_to_db:
+    #         for idx, news in enumerate(news_list):
+    #             if idx < len(news_polarity_response):
+    #                 item = news_polarity_response[idx]
+    #                 text_3c = item.get("text", item.get("original", None))
+    #                 assert news.generated_text == text_3c
+    #                 label_3c = item.get("label", item.get("polarity", None))
+    #                 GeneratedNews.objects.filter(pk=news.pk).update(
+    #                     polarity_3c=label_3c
+    #                 )
+    #
+    #     return news_polarity_response
 
     def check_polarity_3c_parallel(
         self, news_list: List[GeneratedNews], num_workers: int = 1
